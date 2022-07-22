8c33a7d798576ca595cbbfb000acfad141499991
zseever <zseever@gmail.com>
2022-07-22 11:45:46 -0700

    Initial set up steps 1 through 10

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+require('dotenv').config();
+require('./config/database');
+
 var app = express();
 
 // view engine setup
@@ -18,6 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('_method'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
