e2256691eb49f62ea15d7ae28623ec570f548dcc
zseever <zseever@gmail.com>
2022-07-24 03:36:02 -0700

    all stock data updated and charting progress

--- controllers/home.js
+++ controllers/home.js
@@ -11,6 +11,27 @@ module.exports = {
     addStock
 }
 
+// function createCandleStickChart(barCount,initialDate) {
+
+// }
+
+
+function index(req, res) {
+    Stock.find({T: { $in: ['AAPL','AMZN','MSFT','META','NVDA','GOOG','WMT','TSLA','GME']}}, function(err, stock) {
+        res.render('home', { stock });
+    })
+    Stock.findOne({}, function(err, stock) {
+        let today = new Date();
+        let yesterday = new Date();
+        yesterday.setDate(today.getDate() - 1);
+        if (stock.daily[stock.daily.length-1].t.toDateString() !== yesterday.toDateString()) {
+            console.log('Updating stocks for new day')
+            fetchDailyStocks();
+        } else {
+            console.log('Stocks are already up to date');
+        }
+    })
+}
 
 function fetchDailyStocks() {
     let today = new Date();
@@ -20,21 +41,17 @@ function fetchDailyStocks() {
     let m = (today.getMonth()+1).length === 2 ? `${today.getMonth()+1}` : `0${today.getMonth()+1}`
     let y = today.getFullYear();
     today = `${y}-${m}-${d}`
-
     fetch(`${rootURL}v2/aggs/grouped/locale/us/market/stocks/${today}?adjusted=true&apiKey=${token}`)
     .then(res => res.json())
     .then(stockData => {
-        addStock(stockData);
+        if (stockData.resultsCount === 0) {
+            console.log('No data for today');
+            return
+        } else {
+            addStock(stockData);
+        }
     })
 }
-
-function index(req, res) {
-    Stock.find({T: { $in: ['AAPL','AMZN','MSFT','META','NVDA','GOOG','WMT','TSLA','GME']}}, function(err, stock) {
-        res.render('home', { stock });
-    })
-    fetchDailyStocks();
-}
-
 
 function addStock(data) {
     let today = new Date();
@@ -71,5 +88,53 @@ function addStock(data) {
                 stock.save();
             }
         })
+    })
+}
+
+/*--- LEAVING THIS TO FETCH INDIVIDUAL STOCK HISTORICAL DATA AS API HAS LIMITATIONS */
+
+function oneTimeFetch() {
+    let today = new Date();    
+    let d = today.getDate()-1;
+    let m = (today.getMonth()+1).length === 2 ? `${today.getMonth()+1}` : `0${today.getMonth()+1}`
+    let y = today.getFullYear();
+    today = `${y}-${m}-${d}`  
+    let ticker = "WMT";
+    fetch(`${rootURL}v2/aggs/ticker/${ticker}/range/1/day/2021-07-22/2022-07-22?adjusted=true&sort=asc&apiKey=${token}`)
+    .then(res => res.json())
+    .then(stockData => {
+        // console.log(stockData.results);
+        let newStock = new Stock({
+            T: ticker,
+            daily: stockData.results.map(s => {
+                return {
+                    c: s.c,
+                    h: s.h,
+                    l: s.l,
+                    n: s.n,
+                    o: s.o,
+                    t: new Date(s.t),
+                    v: s.v,
+                    vw: s.vw,  
+                }
+            })
+        })
+        newStock.save();
+        // Stock.findOne({T:'WMT'}, function(err, stock) {
+        //     let stocksArr = stockData.results.map(s => {
+        //         return {
+        //             c: s.c,
+        //             h: s.h,
+        //             l: s.l,
+        //             n: s.n,
+        //             o: s.o,
+        //             t: new Date(s.t),
+        //             v: s.v,
+        //             vw: s.vw,  
+        //         }
+        //     });
+        //     stock.daily = stocksArr.concat(stock.daily);
+        //     stock.save();
+        // })
     })
 }
