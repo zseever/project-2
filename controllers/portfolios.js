aebfc3d67f163ce6b3aac2aed78ddb574b1f9b16
zseever <zseever@gmail.com>
2022-07-25 11:17:39 -0700

    Portfolio functionality progress

--- controllers/portfolios.js
+++ controllers/portfolios.js
@@ -3,13 +3,35 @@ const User = require('../models/user');
 
 module.exports = {
     index,
-    new: newStock
+    new: newStock,
+    create
 }
 
 function index(req, res) {
-    res.render('portfolios/index');
+    UserStockList.findOne({user:req.user._id}, (err,userLists) => {
+        res.render('portfolios/index', { userLists });
+    })
 }
 
 function newStock(req, res) {
     res.render('portfolios/new');
+}
+
+function create(req, res) {
+    req.body.dateAdded = new Date();
+    UserStockList.findOne({user:req.user._id}, function(err, user) {
+        if (user) {
+            user.portfolio.push(req.body);
+            user.save();
+        } else {
+            const newUserList = new UserStockList({
+                user: req.user._id,
+                userName: req.user.name,
+                userAvatar: req.user.avatar,
+                portfolio: [req.body]
+            })
+            newUserList.save();
+        }
+    })
+    res.redirect('/portfolios');
 }
--- routes/portfolios.js
+++ routes/portfolios.js
@@ -5,6 +5,7 @@ const isLoggedIn = require('../config/auth');
 
 router.get('/', isLoggedIn, portfoliosCtrl.index);
 router.get('/new', isLoggedIn, portfoliosCtrl.new);
+router.post('/', isLoggedIn, portfoliosCtrl.create);
 
 
 module.exports = router;
